7bf153ab00cd4ee98e716f1abd3648be879d73fc
Pattadon Prongpreecha <pattadon.pro@ascendcorp.com>
2021-02-22 03:25:15 +0700

    first commit

---
+++ src/test/java/com/max/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
